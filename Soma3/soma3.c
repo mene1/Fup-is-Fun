d42a5aae4cc666e609c2c5d7edec8d98db1a3003
Renato Menezes <renatoledo@protonmail.com>
2025-10-01 16:02:02 -0300

    Add files via upload

---
+++ Soma3/soma3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int num1 = 0; int num2 = 0; int num3 = 0; int soma = 0;
+    
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    soma = num1 + num2 + num3;
+    
+    printf("Soma: %d\n", soma);
+    
+    return 0;
+}
