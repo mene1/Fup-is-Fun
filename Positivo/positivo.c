129fe869c6c658adb25f6411cabd773842b4e7e9
Renato Menezes <renatoledo@protonmail.com>
2025-10-07 11:25:57 -0300

    Add files via upload

---
+++ Positivo/positivo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1 = 0;
+
+    scanf("%d", &n1);
+
+    if(n1 >= 0){
+        printf("SIM\n");
+        return 0;
+    } else {
+        printf("NAO\n");
+        return 0;
+    }
+}
