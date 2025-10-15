d2702b03412e191bfb162f450836c52ad5eae456
Renato Menezes <renatoledo@protonmail.com>
2025-10-15 01:26:32 -0300

    Update Jokenpo.c

--- Jokenpo/Jokenpo.c
+++ Jokenpo/Jokenpo.c
@@ -6,19 +6,20 @@ int main(){
 
     scanf("%c %c", &op1, &op2);
 
-    // se 1 e 2 sao iguais
-    // se 1 r -> p ou s
-    // se 1 p -> r ou s
-    // se 1 s -> p ou r
-
     if(op1 == op2){
         printf("empate\n");
     } else if(op1 == 'r'){
-        ;
+        if(op2 == 's'){
+            printf("jog1\n");
+        } else printf("jog2\n"); 
     } else if(op1 == 'p'){
-        ;
+        if(op2 == 'r'){
+            printf("jog1\n");
+        } else printf("jog2\n");
     } else if(op1 == 's'){
-        ;
+        if(op2 == 'p'){
+            printf("jog1\n");
+        } else printf("jog2\n");
     }
 
     return 0;
