6cad1e5a06e41d7a31d3f27b69b81e786ed6f7d7
Renato Menezes <renatoledo@protonmail.com>
2025-10-15 01:29:19 -0300

    Update Jokenpo.c
    CASE SENSITIVE MOMENT

--- Jokenpo/Jokenpo.c
+++ Jokenpo/Jokenpo.c
@@ -8,16 +8,16 @@ int main(){
 
     if(op1 == op2){
         printf("empate\n");
-    } else if(op1 == 'r'){
-        if(op2 == 's'){
+    } else if(op1 == 'R'){
+        if(op2 == 'S'){
             printf("jog1\n");
         } else printf("jog2\n"); 
-    } else if(op1 == 'p'){
-        if(op2 == 'r'){
+    } else if(op1 == 'P'){
+        if(op2 == 'R'){
             printf("jog1\n");
         } else printf("jog2\n");
-    } else if(op1 == 's'){
-        if(op2 == 'p'){
+    } else if(op1 == 'S'){
+        if(op2 == 'P'){
             printf("jog1\n");
         } else printf("jog2\n");
     }
